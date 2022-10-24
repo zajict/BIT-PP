3649c2a50d3d1b0d559b2805cefe983dfab88ba3
Tijana Zajic <tijanazajic1204@gmail.com>
2022-10-24 18:01:28 +0200

    Add new exercise

--- Switch and Array/vezba 1/script.js
+++ Switch and Array/vezba 1/script.js
@@ -35,7 +35,7 @@ console.log(result);
 // For input 10, output should be “Input must be a number between 1 and 7”.
 
 var day = 10;
-var result ='';
+var result;
 
 switch (day) {
     case 1: 
@@ -72,7 +72,7 @@ console.log(result);
 // For input 12, output should be “Input must be number between 1 and 7”.
 
 var day = 2;
-var result ='';
+var result;
 
 switch (day) {
     case 1: 
@@ -107,7 +107,7 @@ console.log(result);
 // For input 13, output should be “Input must be a number between 1 and 12”.
 
 var month = 6;
-var result ='';
+var result;
 
 switch (month) {
     case 1: 
@@ -155,7 +155,7 @@ console.log(result);
 // 5.	Write a program that for a 1-12 number input (representing a month in a year) shows what season it is. All inputs different from 1-12 output a message explaining that input must be a number between 1 and 12. 
 
 var month = 5;
-var result ='';
+var result;
 
 switch (month) {
     case 1: 
@@ -187,7 +187,7 @@ console.log(result);
 // 6.	Write a program that for a string input of a grade from “A”-“F” outputs a proper info message about that grade in the following manner: A - "Good job"", B - "Pretty good"", C - "Passed"", D - "Not so good"", F - "Failed". Input different from letters A-F outputs a message "Unknown grade".
 
 var letter = 'B';
-var result = '';
+var result;
 
 switch (letter) {  
     case 'A': 
@@ -218,7 +218,7 @@ switch (letter) {
 // 7.	Write a program that takes as input a city name and outputs the country where the city is. You may choose which cities and countries you want to output yourself, however there has to be at least 5 countries and 15 cities. Note that each country must have a different number of cities. Input different from the listed cities should output a message"Please choose a different city".
 
 var city = 'Belgrade';
-result = '';
+var result;
 
 switch (city) {
     case 'Belgrade':
--- Switch and Array/vezba 2/script.js
+++ Switch and Array/vezba 2/script.js
@@ -1,6 +1,6 @@
 // 1.	Write an array of months in a year. Using console.log display June, October and January from the array.
 
-var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'December'];
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 console.log(month[5], month[9], month[0]);
 
@@ -17,5 +17,39 @@ var number = [2, -4, 5, -2, -11];
 
 console.log(number[1], number[3], number[4]);
 
+console.log('------');
+
+
 
 // 4.	Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9]. 
+
+var number = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+console.log(number[1], number[4], number[5], number[6], number[8]);
+
+console.log(number[3], number[6]);
+
+console.log('------');
+
+
+
+ /*5.	What is the index of number 24 in the following array?
+[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];*/
+var array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+console.log(array[0][3]);
+console.log('[0][3]');
+
+console.log('------');
+
+/*Using console.log:
+●	Display the 3rd element of the array,
+●	Display the 2nd element of the 3rd element. */
+
+
+console.log(array[2]);
+console.log(array[2][1]); 
+
+
+
+
