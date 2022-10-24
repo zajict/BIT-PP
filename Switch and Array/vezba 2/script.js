d8f9cf4e9acfa3fe363c387e533fd886ab2d75c5
Tijana Zajic <tijanazajic1204@gmail.com>
2022-10-24 18:28:05 +0200

    Changing exercise

--- Switch and Array/vezba 2/script.js
+++ Switch and Array/vezba 2/script.js
@@ -21,13 +21,11 @@ console.log('------');
 
 
 
-// 4.	Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9]. 
+// 4.	Print all elements with indices, divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9]. 
 
 var number = [5, 15, -5, 20, 12, 18, 72, 14, 9];
 
-console.log(number[1], number[4], number[5], number[6], number[8]);
-
-console.log(number[3], number[6]);
+console.log(number[1], '- index:1,', number[4], '- index:4,', number[5], '- index:5,', number[6], '- index:6,', number[8], '- index:8,');
 
 console.log('------');
 
@@ -37,7 +35,7 @@ console.log('------');
 [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];*/
 var array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 
-console.log(array[0][3]);
+
 console.log('[0][3]');
 
 console.log('------');
