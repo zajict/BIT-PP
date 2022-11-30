f6ee8fe93f98c6fcff63e6f04653bfe2646ff529
Tijana Zajic <tijanazajic1204@gmail.com>
2022-11-30 18:20:30 +0100

    add exercise

--- Function/Function intro/script5.js
+++ Function/Function intro/script5.js
@@ -334,9 +334,6 @@ function checkIfStringISPalindrome(string) {
     }
 }
 var y = checkIfStringISPalindrome("a nut for a jar of tuna");
-
-
-
 console.log(y);
 
 
@@ -344,5 +341,8 @@ console.log(y);
 
 /*   12.	Write a program that calculates the greatest common divisor of two integers. Note: The greatest common divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
 Input:  192 42 | 81 9
-Output: 6      | 9
-*/
+Output: 6      | 9 */
+function calculateGreatestCommonDivisor (a, b) {
+   
+}
+
