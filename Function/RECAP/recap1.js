96a747db8a5f69873e53465f21af26afc361073e
Tijana Zajic <tijanazajic1204@gmail.com>
2022-12-02 21:38:05 +0100

    Add recap

---
+++ Function/RECAP/recap1.js
@@ -0,0 +1,227 @@
+// Example 0
+var result = true;
+function subtract(first, second) {
+    return first - second;
+}
+result = subtract(12, -10);
+result = subtract(22, 2);
+
+console.log(result);
+
+
+// Example 1
+'use strict';
+
+myVar = "random";
+
+function addOne(num) {
+    return num + 1;
+}
+var result = addOne(4);
+console.log(result);
+
+
+
+// Example 2
+
+var global = 123;
+
+var resetGlobal = function () {
+    global = -1;
+};
+
+console.log(global);
+
+
+
+
+// Example 3
+
+var global = 123;
+
+var resetGlobal = function () {
+    global = -1;
+};
+
+resetGlobal;
+console.log(global);
+
+
+
+
+// Example 4
+
+var global = 123;
+
+var resetGlobal = function () {
+    global = arguments[0] || -1;
+};
+
+resetGlobal(10);
+console.log(global);
+
+resetGlobal(0);
+console.log(global);
+
+
+
+
+// Example 5
+
+'use strict';
+
+var result = square(10);
+console.log(result);
+
+function square(num) {
+    num *= num;
+}
+
+
+
+// Example 6
+
+'use strict';
+
+var result = square(10);
+console.log(result);
+
+function square(num) {
+    return num *= num;
+}
+
+
+
+
+// Example 7
+
+function toString(num) {
+    result = num + '';
+    return result;
+}
+
+console.log(result);
+
+
+
+
+// Example 8
+
+function toString(num) {
+    'use strict';
+    result = num + '';
+    return result;
+}
+
+toString(num);
+console.log(result);
+
+
+
+
+// Example 9
+
+var output = toString(0.15);
+console.log(output);
+
+var toString = function (num) {
+    var result = num + '';
+    return result;
+}
+
+
+
+
+// Example 10
+
+var toString = function (num) {
+    var result = num + '';
+    return result;
+}
+
+var output = toString(0.15);
+console.log(output);
+
+
+
+
+
+// Example 11
+
+var toString = function (num) {
+    num = num || '';
+    var result = num + '';
+    return result;
+}
+
+var output = toString;
+console.log(output);
+
+
+
+
+// Example 12
+
+// 'use strict';
+
+var global = parseInt("1e2");
+
+incrementGlobal = function () {
+    global++;
+};
+
+incrementGlobal();
+
+console.log(global);
+console.log(typeof global);
+
+
+
+
+
+// Example 13
+
+function sum(num1, num2) {
+    num1 = num1 || 0;
+    num2 = num2 || 0;
+
+    if (! num1 && ! num2) {
+        return -1;
+    }
+
+    return num1 + num2;
+}
+
+var sumNumbers = sum;
+var result = sumNumbers();
+
+console.log(result);
+
+
+
+
+// Example 14
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl();
+
+
+
+
+// Example 14
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl();
+
+
+
+
+
---
+++ Function/RECAP/recap2.js
@@ -0,0 +1,235 @@
+// Example 1
+
+function multiply(a, b) {
+    var mResult = a * b;
+    return function (a, b) {
+        return mResult + a + b;
+    }
+}
+
+var output = multiply(3, 4);
+console.log(output(6, 7));
+
+
+// Example 2
+
+function multiply(a, b) {
+    var mResult = a * b;
+    return function (a, b) {
+        return mResult + a + b;
+    }
+}
+
+var output = multiply(3, 4)(5, 5);
+console.log(output);
+
+
+// Example 3
+
+// print("This might work or not?");
+
+// var print = (function () {
+//     return console.log;
+// })();
+console.log(console.log)
+
+
+// Example 4
+
+var print = (function () {
+    return console.log;
+})();
+
+print("This might work or not?");
+
+
+// Example 5
+
+var input = [
+    1,
+    undefined,
+    2,
+    NaN,
+    3,
+    4,
+    5,
+    "six"
+]
+
+var output = transformArray(input, function (param) {
+    return param + "";
+});
+
+console.log(output);
+
+function transformArray(array, action) {
+    var result = [];
+    for (var index = 0; index < array.length; index++) {
+        var element = array[index];
+        result[index] = action(element);
+    }
+    return result;
+}
+
+
+// Example 6
+
+var PI = 3.1;
+
+function circleSurface(radius) {
+    var result = radius * radius * PI;
+    var PI = 3.14159;
+
+    return result;
+}
+
+var output = circleSurface(5);
+console.log(output);
+
+
+// Example 7
+
+function circleCalc(radius, calculation) {
+    var result = calculation(radius);
+    return result;
+}
+
+var output = circleCalc(7, function (r) {
+    var PI = 3.14159;
+    return 2 * r * PI;
+});
+
+console.log(output);
+
+
+// Example 8
+
+var input = [
+    "one",
+    "1e3",
+    "10e-3",
+    "32",
+    "a15",
+    "12,1"
+]
+
+function transformArray(array, action) {
+    var result = [];
+    for (var index = 0; index < array.length; index++) {
+        var element = array[index];
+        result[index] = action(element);
+    }
+    return result;
+}
+
+var output = transformArray(input, parseFloat);
+console.log(output);
+
+output = transformArray(input, parseInt);
+console.log(output);
+
+
+// Example 10
+
+
+// Moze i ovako da se pise----- zadatak 10
+var cubeVolume = (a) => {
+    return a * a * a;
+}
+
+function cubeCalculation(a, calc) {
+    return calc(a);
+}
+
+output = cubeCalculation(10, cubeVolume)
+console.log(output);
+
+
+// Example 10
+
+var cubeVolume = function (a) {
+    return a * a * a;
+}
+
+function cubeCalculation(a, calc) {
+    return calc(a);
+}
+
+output = cubeCalculation(10, cubeVolume(6))
+console.log(output);
+
+
+// Example 11
+
+function cubeCalculation(a, calc) {
+    return calc(a);
+}
+
+var output = cubeCalculation(10, function (a) {
+    6 * a * a;
+})
+console.log(output);
+
+
+// Example 12
+
+function cubeCalculation(a, calc) {
+    return calc(a);
+}
+
+var output = cubeCalculation(10, function (a) {
+    return 6 * a * a;
+})
+console.log(output);
+
+
+// Example 13
+
+function saySomething(msg1) {
+    return function (msg2) {
+        var output = msg1 + " " + msg2;
+        return function () {
+            output += "!"
+            console.log(output);
+        }
+    }
+}
+
+saySomething("Hi")("there");
+
+
+// Example 13
+
+function saySomething(msg1) {
+    function message(msg2) {
+        var output = msg1 + " " + msg2;
+        var result = function () {
+            output += "!";
+            console.log(output);
+        }
+        return result;
+    }
+    return message;
+}
+
+saySomething("Hi")("there")();
+
+
+// Example 14
+
+var myArr = [];
+
+var hi = function () {
+    return "Hi";
+};
+
+myArr[0] = hi;
+myArr[0]();
+
+console.log(typeof myArr[0])
+var firstElement = myArr[0];
+
+typeof firstElement;
+
+firstElement();
+
