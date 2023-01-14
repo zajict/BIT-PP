1f4b4e7fa2028c49a2ae5eac97248ac8a3486d65
Tijana Zajic <tijanazajic1204@gmail.com>
2023-01-14 14:05:12 +0100

    Change inheritance exercisen

--- Inheritance/Applications.js
+++ Inheritance/Applications.js
@@ -1,14 +1,35 @@
-/*1.	Create constructor functions with properties representing the following:
-○	WebApp: name, url, technologies, licence, stars
-○	MobileApp: name, platforms, licence, stars*/
-
-function WebApp(name, url, technologies, licence, stars) {
+function App(name, licence, stars,) {
     this.name = name;
-    this.url = url;
-    this.technologies = technologies;
-    this. licence = licence;
+    this.licence = licence;
     this.stars = stars;
 }
+
+App.prototype.isCCLicence = function () {
+    if (this.licence === 'CC') {
+        console.log('The licence od the application is CC');
+    } else {
+        console.log('The licence od the application is not CC');
+    }
+}
+
+
+App.prototype.like = function () {
+    this.stars += 1;
+    return this.stars
+}
+
+App.prototype.showStars = function () {
+    console.log(this.stars);
+}
+
+
+function WebApp(name, url, technologies, licence, stars) {
+    App.call(this, name, licence, stars);
+    this.url = url;
+    this.technologies = technologies;
+}
+WebApp.prototype = Object.create(App.prototype);
+WebApp.prototype.constructor = WebApp;
 
 WebApp.prototype.getData = function () {
     console.log(this.name + ' ' + this.url + ' ' + this.technologies + ' ' + this. licence + ' ' + this.stars);
@@ -25,15 +46,14 @@ WebApp.prototype.reactBased = function () {
 
 
 
-
-
-
 function MobileApp(name, platforms, licence, stars) {
-    this.name = name;
+    App.call(this, name, licence, stars);
     this.platforms = platforms;
-    this. licence = licence;
-    this.stars = stars;
 }
+
+MobileApp.prototype = Object.create(App.prototype);
+MobileApp.prototype.constructor = MobileApp;
+
 MobileApp.prototype.getData = function () {
     console.log(this.name + ' ' + this.platforms + ' ' + this.licence + ' ' + this.stars);
 }
@@ -45,17 +65,7 @@ MobileApp.prototype.forAndroids = function () {
     }
 }
 
-MobileApp.prototype.isCCLicence = function () {
-    if (this.licence === 'CC') {
-        console.log('The licence is of application is CC');
-    } else {
-        console.log('The licence is of application is not CC');
-    }
-}
 
-MobileApp.prototype.like = function () {
-    this.stars += 1;
-}
 
 
 
@@ -64,6 +74,11 @@ MobileApp.prototype.like = function () {
 var app = new WebApp('aplikacija', 'url address', 'React', 1234566788, 3.9);
 app.getData();
 app.reactBased();
+app.isCCLicence();
+app.like();
+app.showStars();
+
+
 
 
 var mobApp = new MobileApp('MobileService', 'Ios', 1361632634, 3.5);
@@ -71,10 +86,15 @@ mobApp.getData();
 mobApp.forAndroids();
 mobApp.isCCLicence();
 mobApp.like();
+mobApp.showStars();
+
+
 
 
 
 console.log(app);
 console.log(mobApp);
+
+
 
 
